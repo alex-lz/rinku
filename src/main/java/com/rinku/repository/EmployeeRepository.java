974168de34bafeafb09c6b37e58ad3fbf9e819b4
alex-lz <alexalm2495@gmail.com>
2021-04-05 20:08:30 -0600

    Crear Spring data repository

---
+++ src/main/java/com/rinku/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.rinku.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rinku.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
